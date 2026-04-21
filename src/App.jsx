41f429f79a5125d24d23d6fdde680833d9086206
boticelli <kandelakitina@gmail.com>
2026-04-21 23:31:45 +0300

    Add navbars

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth.jsx";
 import Checkout from "./pages/Checkout.jsx";
+import Navbar from "./pages/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="app">
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/auth" element={<Auth />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
 export default function Home() {
-	return <div>Home page</div>;
+	return (
+		<div className="page">
+			<div className="home-hero">
+				<h1 className="home-title">Welcome to my shop</h1>
+				<p>Discover more products</p>
+			</div>
+			<div className="container">
+				<h2>Our products</h2>
+			</div>
+		</div>
+	);
 }
