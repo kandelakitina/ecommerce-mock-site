71e7888a5de59590a31cf83cf19306f2978a0136
boticelli <kandelakitina@gmail.com>
2026-04-21 12:24:00 +0300

    First commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Simple nodeJS devShell flake";
+
+  inputs.flakelight.url = "github:nix-community/flakelight";
+  outputs = { flakelight, ... }:
+    flakelight ./. {
+      devShell = pkgs: {
+        packages = with pkgs; [ nodejs ];
+        shellHook = ''
+          # Moving npm to local folder instead of nix store
+          npm config set prefix '~/.npm-packages'
+          export PATH="$HOME/.npm-packages/bin:$PATH"
+          export NODE_PATH="$HOME/.npm-packages/lib/node_modules"
+
+          echo "node `${pkgs.nodejs}/bin/node --version`"
+          echo "Shell includes: node"
+        '';
+      };
+    };
+}
