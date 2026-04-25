7100b095870894031fff425707859e6f2213536d
boticelli <kandelakitina@gmail.com>
2026-04-25 17:19:25 +0300

    Add CardContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
 			<AuthProvider>
-				<App />
+				<CartProvider>
+					<App />
+				</CartProvider>
 			</AuthProvider>
 		</BrowserRouter>
 	</StrictMode>,
