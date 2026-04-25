fcfc3896d8d64045844f9a4b638dd0ae2c632aea
boticelli <kandelakitina@gmail.com>
2026-04-25 12:14:21 +0300

    Add email and password validation

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,9 +2,20 @@
 /** biome-ignore-all lint/a11y/useKeyWithClickEvents: no reason */
 
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Auth() {
 	const [mode, setMode] = useState("signup");
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<div className="page">
 			<div className="contrainer">
@@ -12,18 +23,43 @@ export default function Auth() {
 					<h1 className="page-title">
 						{mode === "signup" ? "Sign up" : "Login"}
 					</h1>
-					<form action="" className="auth-form">
+					<form onSubmit={handleSubmit(onSubmit)} className="auth-form">
 						<div className="form-group">
 							<label htmlFor="email" className="form-label">
 								Email
 							</label>
-							<input type="email" className="form-input" id="email" />
+							<input
+								type="email"
+								className="form-input"
+								id="email"
+								{...register("email", { required: "Email is required" })}
+							/>
+							{errors.email && (
+								<span className="form-error">{errors.email.message}</span>
+							)}
 						</div>
 						<div className="form-group">
 							<label htmlFor="password" className="form-label">
 								Password
 							</label>
-							<input type="password" className="form-input" />
+							<input
+								type="password"
+								className="form-input"
+								{...register("password", {
+									required: "Password is required",
+									minLength: {
+										value: 8,
+										message: "Password must be at least 8 characters",
+									},
+									maxLength: {
+										value: 50,
+										message: "Password must be less than 50 characters",
+									},
+								})}
+							/>
+							{errors.password && (
+								<span className="form-error">{errors.password.message}</span>
+							)}
 						</div>
 						<button className="btn btn-primary btn-large" type="submit">
 							{mode === "signup" ? "Sign up" : "Login"}
